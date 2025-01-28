d1fc25e55da4455a37746b78124a01a53f622ed1
shiwaniii <Shiwani.adhikari@gmail.com>
2025-01-28 09:26:54 +0545

    changes in nav

---
+++ newspaper_app/navigation.py
@@ -0,0 +1,7 @@
+from newspaper_app.models import Category, Post, Tag
+
+def nav(request):
+    tags = Tag.objects.all()[:10]
+    categories = Category.objects.all() [:3]
+    trending_posts = Post.objects.filter(published_at__isnull=False, status="active").order_by("-views_count")[:3]
+    return {"tags": tags, "categories": categories, trending_posts: trending_posts}
